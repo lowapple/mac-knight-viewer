261ab5cc042e6a430aa815dd0fef8ebf8847a8ff
Junseo <lowapple99@gmail.com>
2018-01-09 19:30:14 +0900

    Initial Commit

---
+++ Night Viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Night Viewer
+//
+//  Created by LowApple on 2018. 1. 9..
+//  Copyright © 2018년 LowApple. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Night Viewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Night Viewer
+//
+//  Created by LowApple on 2018. 1. 9..
+//  Copyright © 2018년 LowApple. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
